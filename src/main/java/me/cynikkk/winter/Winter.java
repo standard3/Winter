77810d78a39f67263bc4dd6d07f6d1f89ae55028
Théo <theo.abel53@gmail.com>
2022-10-06 22:25:03 +0200

    Repository init

---
+++ src/main/java/me/cynikkk/winter/Winter.java
@@ -0,0 +1,12 @@
+package me.cynikkk.winter;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Winter implements ModInitializer
+{
+	@Override
+	public void onInitialize()
+	{
+
+	}
+}
---
+++ src/main/java/me/cynikkk/winter/client/WinterClient.java
@@ -0,0 +1,15 @@
+package me.cynikkk.winter.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class WinterClient implements ClientModInitializer
+{
+	@Override
+	public void onInitializeClient()
+	{
+
+	}
+}
